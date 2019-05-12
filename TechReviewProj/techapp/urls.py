931f606459270ce14768aba55a82eae59a6d9ba2
Sally <46608728+SRAdda@users.noreply.github.com>
2019-05-11 20:08:40 -0700

    Update urls.py

--- TechReviewProj/techapp/urls.py
+++ TechReviewProj/techapp/urls.py
@@ -5,5 +5,10 @@ urlpatterns=[
     path('', views.index,name='index'),
     path('getTypes/', views.getTypes, name='types'),
     path('getProducts/', views.getProducts, name='products'),
-    path('productdetail/<int:id>',views.productdetail, name='details')
+    path('productdetail/<int:id>', views.productdetail, name='productdetail'),
+    path('productreviews/<int:prod_id>',views.productreviews, name='productreviews'),
+    path('newProduct/', views.newProduct, name='newProduct'),
+    path('newReview/', views.newReview, name='newReview'),
+    path('logoutmessage', views.logoutmessage, name='message'),
+    path('loginmessage', views.loginmessage, name='welcome'),
 ]
